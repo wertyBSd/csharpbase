2a2a58f31f25841ac2fd6b455435734cb0eecc6f
wertyBSd <wertybde@gmail.com>
2024-02-28 22:00:28 +0300

    lesson11

---
+++ CSharp/CSharpGames/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++)
+        {
+            sb.AppendLine($"i={i.ToString()}");
+        }
+        Console.WriteLine( sb.ToString() );
+        Console.ReadKey();
+    }
+}
